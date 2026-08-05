f5aa7ffc97e038b64dbdd8125db76dbbf0fd6241
Spinel24x <spinelapper@gmail.com>
2026-08-05 04:23:08 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,16 @@ set -e
 
 mkdir -p /app/configs /app/data /var/log/nginx /var/lib/nginx /tmp/nginx
 
+# Generate Xray config first
+python3 -c "from main import generate_xray_config; generate_xray_config()"
+
 # Start Xray
 xray run -config /app/configs/xray.json &
 sleep 1
 
 # Start API server
 python3 /app/main.py &
-sleep 1
+sleep 2
 
 # Start Nginx
 exec nginx -c /app/nginx.conf -g "daemon off;"
