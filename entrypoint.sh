eea8b48b1d2e7dd8473a95f51c5005ea7115f83f
Spinel24x <spinelapper@gmail.com>
2026-08-05 10:38:03 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-set -e
-
 mkdir -p /app/configs /app/data /var/log/nginx /var/lib/nginx /tmp/nginx
 
-# Start Xray first
-xray run -config /app/configs/xray.json &
-sleep 2
-
-# Start API
+# Start API server
 cd /app
 python3 main.py &
 sleep 3
 
-# Start Nginx last
+# Start Nginx
 exec nginx -c /app/nginx.conf -g "daemon off;"
