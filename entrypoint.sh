d0e8dd045a4110c96cc6ac691d3c70d0ccbdc059
Spinel24x <spinelapper@gmail.com>
2026-08-05 10:47:28 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 mkdir -p /app/configs /app/data /var/log/nginx /var/lib/nginx /tmp/nginx
-
-# Start API server
 cd /app
 python3 main.py &
 sleep 3
-
-# Start Nginx
 exec nginx -c /app/nginx.conf -g "daemon off;"
